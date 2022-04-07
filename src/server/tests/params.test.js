5976e27874e269ebd22ab98c5c92eb46af4de012
Lev <lev.barsegyan@devit-team.com>
2022-04-07 13:16:06 +0000

    add test on params

---
+++ src/server/tests/params.test.js
@@ -0,0 +1,12 @@
+describe('params', () => {
+  const ENV = Object.assign({}, process.env);
+  afterEach(() => process.env = ENV);
+  it('should match test params', () => {
+    process.env.GIT_HASH = '#test';
+    expect(require('../params')).toEqual({
+      gitHash: '#test',
+      env: 'test',
+      server: { host: 'localhost' },
+    });
+  });
+});
